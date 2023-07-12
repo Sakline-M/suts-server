1879178168873594220238ed6a92647180ff37db
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-07-12 08:38:17 -0600

    biplob shop done

--- controller/courseController.js
+++ controller/courseController.js
@@ -15,9 +15,36 @@ export const getAllCourse = async (req, res) => {
       sort = { name: 1 };
     }
     // calculate category
-    
+    let category = {};
+    if (req.query.category != "All") {
+      category = req.query.category;
+    }
+    // calculate hightest and lowest
+    let price = { $gt: req.query.gt, $lt: req.query.lt };
 
-    const courses = await Course.find({}).sort(sort);
+    // calculate query
+    let query = {};
+    if (req.query.category === "All") {
+      query = { price };
+    }
+    if (req.query.category != "All") {
+      category = req.query.category;
+      query = { category, price };
+    }
+
+    // calculate pagenation
+    let skip = 0;
+    let limit = 15;
+    if (req.query.category === "All") {
+      const page = req.query.page;
+      const limit = req.query.limit;
+      skip = (page - 1) * parseInt(limit);
+    }
+    
+    // query
+    const courses = await Course.find(query).sort(sort).skip(skip).limit(limit);
+
+    // send to client
     res.send(courses);
   } catch (error) {
     console.log(error);
